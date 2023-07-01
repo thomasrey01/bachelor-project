f3abf3f7cce6b848583459993adb22977b523c8d
thomas <thomas@pop-os.localdomain>
2023-07-01 15:21:49 +0200

    to keep up to date

--- scripts/populate_csv.py
+++ scripts/populate_csv.py
@@ -1,8 +1,14 @@
 import csv
+import yaml
 
-with open('./data/shelves.csv', 'w', encoding='UTF8') as f:
-    writer = csv.writer(f)
-    header = ['Tag', 'x0', 'x1', 'y0', 'y1']
-    writer.writerow(header)
+# with open('./data/shelves.csv', 'w', encoding='UTF8') as f:
+#     writer = csv.writer(f)
+#     header = ['Tag', 'x0', 'x1', 'y0', 'y1']
+#     writer.writerow(header)
 
+with open('./config.yaml') as f:
+    data_map = yaml.safe_load(f)
+    num_groups = data_map['number_groups']
+    for i in range(num_groups):
+        print("so")
     
